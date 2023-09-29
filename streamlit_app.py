53b9bdb465a1df81e2e4c0f7013a9bd59db176d8
torres211 <146489938+torres211@users.noreply.github.com>
2023-09-29 15:36:03 -0600

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega')
+streamlit.text('Khale')
+streamlit.text('Hard-')
+
